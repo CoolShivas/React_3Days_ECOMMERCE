fae1827bcfd18f10ef0127c05c8b01bc7138ab56
CoolShivas <shivas2710cool00@gmail.com>
2024-07-08 11:51:11 +0530

    Posting data on firebase rest api from AddProduct Page.

--- ecomm_project/src/pages/AddProducts.jsx
+++ ecomm_project/src/pages/AddProducts.jsx
@@ -1,24 +1,73 @@
+import { useState } from "react";
 import styles from "./AddProducts.module.css";
 
 
 const AddProducts = () => {
+
+    const [products, setProducts] = useState({
+        name: "",
+        image: "",
+        details: "",
+        price: "",
+    });
+
+    const handlerOnChange = (e) => {
+        setProducts({
+            ...products, [e.target.id]: e.target.value
+        })
+    };
+
+    const handlerOnSubmitForm = async (e) => {
+        e.preventDefault();
+        console.log(products);
+
+        try {
+            const response = await fetch(`https://daysecommerce01-default-rtdb.firebaseio.com/eCommerce.json`, {
+                method: "POST",
+                body: JSON.stringify(products),
+            });
+
+            if (!response.ok) {
+                throw new Error("Something went wrong with add products");
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
+        setProducts({
+            name: "",
+            image: "",
+            details: "",
+            price: "",
+        });
+
+    };
+
     return (
         <div className={styles.add_products__container}>
-            <form action="#">
+            <form onSubmit={handlerOnSubmitForm}>
                 <div>
                     <label htmlFor="name"> Product Name </label>
-                    <input type="text" id="name" />
+                    <input type="text" id="name" onChange={handlerOnChange} />
+                </div>
+                <div>
+                    <label htmlFor="image"> Product Image </label>
+                    <input type="link" id="image" onChange={handlerOnChange} />
                 </div>
                 <div>
                     <label htmlFor="details"> Product Details </label>
-                    <input type="text" id="details" />
+                    <input type="text" id="details" onChange={handlerOnChange} />
                 </div>
                 <div>
                     <label htmlFor="price"> Product Price </label>
-                    <input type="number" id="price" />
+                    <input type="number" id="price" onChange={handlerOnChange} />
                 </div>
                 <div className={styles.actions}>
-                    <button className="btn btn-info"> Add to products </button>
+                    <button className="btn btn-info" type="submit"> Add to products </button>
                 </div>
             </form>
         </div>
