8034c072a596f56ede000da81c3e355402468e98
CoolShivas <shivas2710cool00@gmail.com>
2024-07-10 01:21:04 +0530

    Sticking on the same page after refreshing the page with the help of idToken stored in LocalStorage.

--- ecomm_project/src/App.jsx
+++ ecomm_project/src/App.jsx
@@ -1,8 +1,9 @@
 import Authentication from "./assets/Authentication";
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import MainPage from "./components/MainPage";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogIn } from "./store/reduxStore";
 
 
 function App() {
@@ -10,6 +11,17 @@ function App() {
   // const [isLogin] = useState(true); // Local state variable;
 
   const isUserLoggedIn = useSelector((store) => store.authentication.isUserLoggedIn);
+
+  const localToken = localStorage.getItem("Save-Token");
+  // console.log(localToken); // Getting the token on console screen;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localToken) {
+      dispatch(setLogIn(localToken));
+    }
+  }, [localToken]);
 
   return <>
     <div className="app_container">
